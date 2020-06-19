39585409609d54312a76c7678a62eb733b45c026
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2020-06-19 19:18:23 -0400

    Added Create React App Template.

---
+++ game-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="hello-world">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          "Hello world!!"
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;
